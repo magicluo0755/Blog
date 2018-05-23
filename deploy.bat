32fe3a4a70e7a39a21e566b131cf12befea95937
luoxianqiao <luo.xianq@gmail.com>
2018-05-23 15:26:22 +0800

    update the build scripts

--- deploy.bat
+++ deploy.bat
@@ -7,31 +7,26 @@
 @echo off
 echo Deploying updates to Github
 
-set HUGO_SITE_ROOT=%cd%
-echo HUGO_SITE_ROOT:  %HUGO_SITE_ROOT%
+::set HUGO_SITECDOE_ROOT=%cd%
+::echo HUGO_SITECODE_ROOT:  %HUGO_SITECODE_ROOT%
 
 set d=%date:~0,10%
 set t=%time:~0,8%
+set commitmsg=rebuidling site %d% %t%
 
-set  commitmsg=rebuidling site %d% %t%
-
-REM commit changes to https://github.com/magicluo0755/my-hugo-blog.git
-
+::Commit changes to https://github.com/magicluo0755/my-hugo-blog.git
 git add -A
 git commit -m "%commitmsg%"
 git push origin master
 
-REM Build the site project
+echo Commit site code changes to https://github.com/magicluo0755/my-hugo-blog.git [Done]
 
-hugo --theme=beautifulhugo
+:: Build the site project
+cd ../magicluo0755.github.io
+set HUGO_SITE_ROOT=%cd%
 
-cd public
-REM git add -A
-REM git commit -m "%commitmsg%"
+hugo --theme=beautifulhugo --destination=%HUGO_SITE_ROOT%
 
-REM Push source and build repos
-REM git push origin master
-
-cd ../
+echo Build site. [Done]
 
 pause
